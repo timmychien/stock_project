c04c07fe7e72aa143d0cfbba42f85fa47460d4e8
timmychien <chien871101@gmail.com>
2021-11-25 11:11:45 +0800

    origin

--- routes/personal/mywork.js
+++ routes/personal/mywork.js
@@ -8,7 +8,8 @@ router.get('/', function (req, res) {
     }
     var pool=req.connection;
     //var name=req.session.userfirstname+req.session.userlastname;
-    var walletaddress=req.session.walletaddress
+    var walletaddress=req.session.walletaddress;
+    var now = parseInt(Date.now() / 1000);
     pool.getConnection(function(err,connection){
         connection.query('SELECT * FROM art_works WHERE authoraddress=? AND available=?', [walletaddress,0],function(err,rows){
             if (err) {
@@ -18,12 +19,30 @@ router.get('/', function (req, res) {
                 })
             }else{
                 var data=rows;
+                connection.query('SELECT * FROM voting WHERE startAddstamp<? AND endAddstamp>?', [now, now], function (err, rows) {
+                    if (err) {
+                        res.render('error', {
+                            message: err.message,
+                            error: err
+                        })
+                    } else {
+                        var topics = rows;
+                        res.render('personal/mywork', {
+                            title: '我的作品',
+                            data: data,
+                            email: req.session.email,
+                            role: req.session.role,
+                            topics: topics,
+                        });
+                    }
+                })
+                /*
                 res.render('personal/mywork', {
                     title: '我的作品',
                     data:data,
                     email: req.session.email,
                     role: req.session.role,
-                });
+                });*/
             }
         })
         connection.release();

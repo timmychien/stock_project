d2d2dca4cc4c39e13b6d749f9ede5eabaa89f0e5
timmychien <chien871101@gmail.com>
2021-12-09 17:30:20 +0800

    update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,19 +19,27 @@ window.addEventListener('load', async () => {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
     web3.eth.getAccounts(function(error,accounts){
-        web3.eth.getBalance(accounts[0], function (error, bal) {
-            if (!error) {
-                $('#addr').text(accounts[0]);
-                $('#bal').text(bal / 10 ** 18);
-                pointcontract.balanceOf(accounts[0],function(err,points){
-                    $('#point_bal').text(points)
-                })
-                
-            } else {
-                console.log(error)
-            }
-        })
+        var walletdb=$('#walletdb').val();
+        if(walletdb!=undefined&& accounts[0]!=walletdb){
+            alert('您目前連接的錢包地址與本帳戶不符！')
+        }
+        if (accounts[0] == walletdb){
+            web3.eth.getBalance(accounts[0], function (error, bal) {
+                if (!error) {
+                    $('#addr').text(accounts[0]);
+                    $('#bal').text(bal / 10 ** 18);
+                    pointcontract.balanceOf(accounts[0], function (err, points) {
+                        $('#point_bal').text(points)
+                    })
+
+                } else {
+                    console.log(error)
+                }
+            })
+        }
     })
+
+    /*
     $('#createSign').click(function(){
         var token = $('#tokenAddr').val();
         var transferSig = "0x48664c16";
@@ -105,5 +113,5 @@ window.addEventListener('load', async () => {
         })
         return false;
     })
-        
+        */
 })
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Home',
-    email: req.session.email,
-    role: req.session.role,
-  });
+  if(req.session){
+    res.render('index', {
+      title: 'Home',
+      email: req.session.email,
+      role: req.session.role,
+      walletaddress: req.session.walletaddress
+    });
+  }
+  else{
+    es.render('index', {
+      title: 'Home',
+    });
+  }
   
 });
 

7c373a5d3500bfc3c069cab87955700f8d20edc5
timmychien <chien871101@gmail.com>
2021-12-13 19:23:58 +0800

    update

--- public/javascripts/navigator.js
+++ public/javascripts/navigator.js
@@ -15,3 +15,31 @@ window.onclick = function (event) {
         }
     }
 }
+
+window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+    web3.eth.getAccounts(function (error, accounts) {
+        pointcontract.balanceOf(accounts[0], function (err, points) {
+            var bal=document.getElementById('pointbal');
+            bal.value=points;
+        })
+
+    })
+})

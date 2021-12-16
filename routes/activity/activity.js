bc3cac55aac7f0e5c9e30e771aadc8c5be619286
timmychien <chien871101@gmail.com>
2021-12-16 14:10:30 +0800

    update

--- routes/activity/activity.js
+++ routes/activity/activity.js
@@ -3,11 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('activity/activity', {
-        title: '活動總覽',
-        email: req.session.email,
-        role: req.session.role,
-    });
+    var pool = req.connection;
+    pool.getConnection(function (err, connection) {
+        var now = parseInt(Date.now() / 1000);
+        connection.query('SELECT * FROM voting WHERE startAddstamp<? AND endAddstamp>?', [now, now], function (err, rows) {
+            if (err) {
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                })
+            }
+            else {
+                var data = rows;
+                res.render('activity/activity', {
+                    title: '活動總覽',
+                    data: data,
+                    email: req.session.email,
+                    role: req.session.role
+                });
+            }
+        })
+        connection.release();
+    })
 
 });
 

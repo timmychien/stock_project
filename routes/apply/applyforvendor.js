d3a33ad8fda5e06663b1262d7bf4d68e9a7dd27f
timmychien <chien871101@gmail.com>
2022-02-18 02:16:43 +0800

    upgrade

--- routes/apply/applyforvendor.js
+++ routes/apply/applyforvendor.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 router.get('/',function(req,res){
-    res.render('apply/applyforvendor',{
-        title:'申請成為商家',
-        email: req.session.email,
-        role: req.session.role,
-        walletaddress: req.session.walletaddress
-    })
+    if (req.session.role == 'vendor') {
+        res.render('apply/apply_warn', {
+            warn: '此帳號已升級！'
+        })
+    }else{
+        res.render('apply/applyforvendor', {
+            title: '申請成為商家',
+            email: req.session.email,
+            role: req.session.role,
+            walletaddress: req.session.walletaddress
+        })
+    }
+    
 })
 router.post('/',function(req,res){
     var pool=req.connection;
--- routes/vendor/worklisting.js
+++ routes/vendor/worklisting.js
@@ -3,11 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('vendor/workListing', {
-        title: 'Home',
+    var pool=req.connection;
+    var vendor=req.session.walletaddress;
+    pool.getConnection(function(err,connection){
+        connection.query('SELECT name FROM collectionlist WHERE vendor=?',[vendor],function(err,rows){
+            var names=rows;
+            res.render('vendor/collectionListing', {
+                title: '商品上架',
+                names:names,
+                email: req.session.email,
+                role: req.session.role,
+                walletaddress: req.session.walletaddress
+            });
+        })
+    })
+});
+router.post('/', function (req, res) {
+    var collection= req.body['select_collection'];
+    res.redirect('/workListing/' + collection);
+})
+router.get('/:collection',function(req,res){
+    var collection=req.params.collection;
+    res.render('vendor/workListing',{
+        title: '商品上架',
+        collection_name:collection,
         email: req.session.email,
         role: req.session.role,
         walletaddress: req.session.walletaddress
-    });
-});
+    })
+})
 module.exports = router;

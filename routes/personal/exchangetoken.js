dbfa0af25a170900adbc3b865634909ea24c18f6
timmychien <chien871101@gmail.com>
2022-03-08 15:58:04 +0800

    update

--- routes/personal/exchangetoken.js
+++ routes/personal/exchangetoken.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Tx = require('ethereumjs-tx').Transaction;
 var Web3 = require('web3');
+var Common = require('ethereumjs-common').default;
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider("https://rinkeby.infura.io/v3/991b420c343949d991d7de33d4d75717"));
+web3.setProvider(new web3.providers.HttpProvider("https://besu-nft-f1da896e4e-node-f6ee1078.baas.twcc.ai"));
 var abi = require('../pointABI');
 var abi = abi.pointABI;
-var pointAddress = "0x99fDf0C3D582520614B49e967929156EF9e4Cbba";
+var pointAddress = "0x3321432994311cf7ee752971C8A8D67dF357fa43";
 var contract = web3.eth.contract(abi).at(pointAddress);
+const customCommon = Common.forCustomChain('mainnet', {
+    name: 'nft',
+    chainId: 13144,
+    networkId: 13144
+
+}, 'petersburg')
 /* GET home page. */
 router.get('/', function (req, res) {
     if (!req.session.email) {
@@ -38,8 +45,8 @@ router.post('/',function(req,res){
         var privkey = Buffer.from(process.env.PRIV_KEY, 'hex');
         var count = web3.eth.getTransactionCount(address);
         var data = contract.operatorMint.getData(toaddress, tochange, '0x', '0x', { from: address });
-        var gasPrice = web3.eth.gasPrice*2;
-        var gasLimit = 8000000;
+        var gasPrice = 0;
+        var gasLimit = 3000000;
         var rawTx = {
             "from": address,
             "nonce": web3.toHex(count),
@@ -48,9 +55,9 @@ router.post('/',function(req,res){
             "to": pointAddress,
             "value": 0x0,
             "data": data,
-            "chainId": 0x04
+            "chainId": 13144
         }
-        var tx = new Tx(rawTx, { chain: 'rinkeby' });
+        var tx = new Tx(rawTx, { common:customCommon});
         tx.sign(privkey);
         var serializedTx = tx.serialize();
         var hash = web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'));

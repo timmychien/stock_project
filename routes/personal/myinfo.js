8b1b18d40df7b7b74cc8d609e64407c391459b25
timmychien <chien871101@gmail.com>
2022-03-31 00:55:51 +0800

    update

--- routes/personal/myinfo.js
+++ routes/personal/myinfo.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
     }
     if (req.session.isverified == 0) {
         console.log('need verify')
-        res.redirect('/verify');
+        res.redirect('/emailverify');
     }
     res.render('personal/myinfo', {
         title: '我的個人資料',

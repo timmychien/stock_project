c1528c0397ce6803fe23e2058ef960a1b59e39cb
timmychien <chien871101@gmail.com>
2022-04-22 02:53:56 +0800

    update_0422

--- routes/personal/mycollection.js
+++ routes/personal/mycollection.js
@@ -1,23 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */ /* GET home page. */
+/* GET home page. */
 router.get("/", function (req, res) {
     if (!req.session.email) {
-            res.redirect("/login");
+        res.redirect("/login");
     }
+
     if (req.session.isverified == 0) {
         if (req.session.isverified == 0) {
-            console.log("need verify");
-            res.redirect("/emailverify");
+            if (req.session.isverified == 0) {
+                console.log("need verify");
+                res.redirect("/emailverify");
+            }
         }
+        res.render("personal/mycollection", {
+            title: "我的收藏品",
+            email: req.session.email,
+            role: req.session.role,
+            mycollections: mycollections,
+        });
     }
-    res.render("personal/mycollection", {
-        title: "我的收藏品",
-        email: req.session.email,
-        role: req.session.role,
-        mycollections: mycollections,
-    });
 });
 
 module.exports = router;

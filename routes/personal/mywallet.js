b1442aa2e24f0d761ae27605aa8a9ddd44483a56
timmychien <chien871101@gmail.com>
2022-05-13 02:10:48 +0800

    update_0513

--- routes/personal/mywallet.js
+++ routes/personal/mywallet.js
@@ -9,15 +9,28 @@ var pointabi = pointabi.pointABI;
 var pointAddress = "0x1e8B628Da1EBcE9B1adA7CD181cda91614762414";
 var pointcontract = web3.eth.contract(pointabi).at(pointAddress);
 router.get('/',function(req,res){
+    var pool=req.connection;
     if (!req.session.email) {
         res.redirect('/login');
     } else {
         var bal = pointcontract.balanceOf(req.session.walletaddress).toNumber();
-        res.render('personal/mywallet', {
-            email: req.session.email,
-            role: req.session.role,
-            bal: bal
+        pool.getConnection(function(err,connection){
+            connection.query('SELECT * FROM point_transactions WHERE address=?',[req.session.walletaddress],function(err,rows){
+                if(err){
+                    console.log(err)
+                }else{
+                    var transactions=rows;
+                    res.render('personal/mywallet', {
+                        email: req.session.email,
+                        role: req.session.role,
+                        bal: bal,
+                        transactions:transactions
+                    })
+                }
+            })
+            connection.release();
         })
+        
     }
 })
 module.exports=router;

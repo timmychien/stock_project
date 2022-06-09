e1378377addb729c1fb135cb55b2e7b361f7169e
timmychien <chien871101@gmail.com>
2022-06-10 00:45:56 +0800

    update_buy_confirm

--- routes/explore/explore_detail.js
+++ routes/explore/explore_detail.js
@@ -91,86 +91,90 @@ router.get("/:contractaddress/:tokenid", function (req, res) {
 
 });
 router.post('/:contractaddress/:tokenid',function(req,res){
-    var contractaddress=req.params.contractaddress;
-    var tokenid=req.params.tokenid;
-    res.render('explore/buy_confirm',{
-        contractaddress:contractaddress,
-        tokenid:tokenid
-    });
+    var contractaddress = req.params.contractaddress;
+    var tokenid = req.params.tokenid;
+    var price = req.body['price'];
+    var bal = pointcontract.balanceOf.call(req.session.walletaddress).toNumber();
+    if (bal < price) {
+        res.render('explore/explore_detail', {
+            warn: '您的點數餘額不足!'
+        })
+    }
+    else{
+        res.render('explore/buy_confirm', {
+            contractaddress: contractaddress,
+            tokenid: tokenid,
+            price: price
+        });
+    }
 })
-router.post('/:contractaddress/:tokenid/confirm', function (req, res) {
+router.post('/:contractaddress/:tokenid/:price/confirm', function (req, res) {
     var bal = pointcontract.balanceOf.call(req.session.walletaddress).toNumber();
     var tokenaddress = req.params.contractaddress;
     var tokenid = req.params.tokenid;
     var price=req.body['price'];
-    if(bal<price){
-        res.render('explore/explore_detail', {
-            warn: '您的點數餘額不足!'
-        })
-    }else{
-        var pool = req.connection;
-        var time = new Date();
-        time = time.getUTCFullYear() + '-' +
-            ('00' + (time.getMonth() + 1)).slice(-2) + '-' +
-            ('00' + time.getDate()).slice(-2) + ' ' +
-            ('00' + time.getHours()).slice(-2) + ':' +
-            ('00' + time.getMinutes()).slice(-2) + ':' +
-            ('00' + time.getSeconds()).slice(-2);    
-        var tokencontract = web3.eth.contract(collectionabi).at(tokenaddress);
-        var name = tokencontract.name.call();
-        var tokenowner = tokencontract.ownerOf.call(tokenid);
-        var buyer = req.session.walletaddress;
-        var address = req.session.walletaddress;
-        var privkey = Buffer.from(req.session.pk, 'hex');
-        var data = vendorcontract.buy.getData(buyer, price, tokenaddress, tokenid);
-        var count = web3.eth.getTransactionCount(address);
-        var gasPrice = web3.eth.gasPrice.toNumber() * 2;
-        var gasLimit = 3000000;
-        var rawTx = {
-            "from": address,
-            "nonce": web3.toHex(count),
-            "gasPrice": web3.toHex(gasPrice),
-            "gasLimit": web3.toHex(gasLimit),
-            "to": vendorAddress,
-            "value": 0x0,
-            "data": data,
-            "chainId": 13144
-        }
-        var tx = new Tx(rawTx, { common: customCommon });
-        tx.sign(privkey);
-        var serializedTx = tx.serialize();
-        var hash = web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'));
-        console.log(hash)
-        pool.getConnection(function (err, connection) {
-            connection.query('DELETE FROM goods_onsell WHERE contract=? AND tokenid=?', [tokenaddress, tokenid], function (err, rows) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    var buyer_bal = pointcontract.balanceOf.call(req.session.walletaddress).toNumber();
-                    buyer_bal=buyer_bal-price;
-                    var seller_bal = pointcontract.balanceOf.call(tokenowner).toNumber();
-                    seller_bal=seller_bal+parseInt(price);
-                    var buy_change=-price;
-                    var sell_change=+price;
-                    var buy_info='購買NFT:'+name+'(id:'+tokenid+')';
-                    var sell_info = '販售NFT:' + name + '(id:' + tokenid + ')';
-                    connection.query('INSERT INTO point_transactions(time,address,hash,change_amount,balance,info)VALUES(?,?,?,?,?,?),(?,?,?,?,?,?)',[time,tokenowner,hash,sell_change,seller_bal,sell_info,time,buyer,hash,buy_change,buyer_bal,buy_info],function(err,rows){
-                        if(err){
-                            console.log(err)
-                        }else{
-                            connection.query("INSERT INTO nft_transaction(txhash,contractAddress,tokenId,actor,info,time)VALUES(?,?,?,?,?,?)", [hash, tokenaddress, tokenid, req.session.name, "購買", time], function (err, rows) {
-                                res.render('explore/buy_redirect');
-                            })
-                            
-                        }
-                    })
-                    
-                }
-            })
-            connection.release();
-        })
-       res.render('explore/buy_redirect');
+    var pool = req.connection;
+    var time = new Date();
+    time = time.getUTCFullYear() + '-' +
+        ('00' + (time.getMonth() + 1)).slice(-2) + '-' +
+        ('00' + time.getDate()).slice(-2) + ' ' +
+        ('00' + time.getHours()).slice(-2) + ':' +
+        ('00' + time.getMinutes()).slice(-2) + ':' +
+        ('00' + time.getSeconds()).slice(-2);    
+    var tokencontract = web3.eth.contract(collectionabi).at(tokenaddress);
+    var name = tokencontract.name.call();
+    var tokenowner = tokencontract.ownerOf.call(tokenid);
+    var buyer = req.session.walletaddress;
+    var address = req.session.walletaddress;
+    var privkey = Buffer.from(req.session.pk, 'hex');
+    var data = vendorcontract.buy.getData(buyer, price, tokenaddress, tokenid);
+    var count = web3.eth.getTransactionCount(address);
+    var gasPrice = web3.eth.gasPrice.toNumber() * 2;
+    var gasLimit = 3000000;
+    var rawTx = {
+        "from": address,
+        "nonce": web3.toHex(count),
+        "gasPrice": web3.toHex(gasPrice),
+        "gasLimit": web3.toHex(gasLimit),
+        "to": vendorAddress,
+        "value": 0x0,
+        "data": data,
+        "chainId": 13144
     }
+    var tx = new Tx(rawTx, { common: customCommon });
+    tx.sign(privkey);
+    var serializedTx = tx.serialize();
+    var hash = web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'));
+    console.log(hash)
+    pool.getConnection(function (err, connection) {
+        connection.query('DELETE FROM goods_onsell WHERE contract=? AND tokenid=?', [tokenaddress, tokenid], function (err, rows) {
+            if (err) {
+                console.log(err)
+            } else {
+                var buyer_bal = pointcontract.balanceOf.call(req.session.walletaddress).toNumber();
+                buyer_bal=buyer_bal-price;
+                var seller_bal = pointcontract.balanceOf.call(tokenowner).toNumber();
+                seller_bal=seller_bal+parseInt(price);
+                var buy_change=-price;
+                var sell_change=+price;
+                var buy_info='購買NFT:'+name+'(id:'+tokenid+')';
+                var sell_info = '販售NFT:' + name + '(id:' + tokenid + ')';
+                connection.query('INSERT INTO point_transactions(time,address,hash,change_amount,balance,info)VALUES(?,?,?,?,?,?),(?,?,?,?,?,?)',[time,tokenowner,hash,sell_change,seller_bal,sell_info,time,buyer,hash,buy_change,buyer_bal,buy_info],function(err,rows){
+                    if(err){
+                        console.log(err)
+                    }else{
+                        connection.query("INSERT INTO nft_transaction(txhash,contractAddress,tokenId,actor,info,time)VALUES(?,?,?,?,?,?)", [hash, tokenaddress, tokenid, req.session.name, "購買", time], function (err, rows) {
+                            res.render('explore/buy_redirect');
+                        })
+                        
+                    }
+                })
+                    
+            }
+        })
+        connection.release();
+    })
+    res.render('explore/buy_redirect');
     
 })
 
